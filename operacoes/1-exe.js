be06c8ca9a029b0fd897fa1030350e95e30991cd
Aline Souza <alinebeatriz115@gmail.com>
2025-05-16 15:25:52 -0300

    exercícios básicos concluidos

---
+++ operacoes/1-exe.js
@@ -0,0 +1,55 @@
+// 1º Some dois números e exiba o resultado.
+let a = 20;
+let b = 50;
+calculo = a + b;
+console.log("Soma: " + calculo);
+
+// 2º Subtraia dois números e exiba o resultado.
+let c = 100;
+let d = 50;
+calculo = c - d;
+console.log("Subtração: " + calculo);
+
+// 3º Multiplique dois números e exiba o resultado.
+let e = 9;
+let f = 11;
+calculo = e * f;
+console.log("Multiplicação: " + calculo);
+
+// 4º Divida dois números e exiba o resultado.
+let g = 65;
+let h = 30;
+calculo = g / h;
+console.log("Dividir: " + calculo);
+
+// 5º Calcule o dobro de um número.
+let i = 20;
+calculo = 2 * i;
+console.log("O dobro de " + i + " é: " + calculo);
+
+// 6º Calcule a média entre dois números.
+let j = 10;
+let l = 6;
+calculo = (j + l) / 2;
+console.log("A média é: " + calculo);
+
+// 7º Verifique se um número é par ou ímpar.
+let m = 4;
+calculo = (m % 2 === 0) ? "Par" : "Ìmpar";
+console.log("O número é: " + calculo);
+
+// 8º Calcule o quadrado de um número. (multiplicar esse número por ele mesmo.)
+let n = 10;
+calculo = n * n;
+console.log("O quadrado do número é: " + calculo);
+
+// 9º Mostre o resto da divisão entre dois números.
+let o = 50;
+let p = 20;
+calculo = (o % p);
+console.log("Resto: " + calculo);
+
+// 10º Converta uma idade em anos para dias.
+let q = 27;
+calculo = q * 365;
+console.log("Dias de vidas: " + calculo);
