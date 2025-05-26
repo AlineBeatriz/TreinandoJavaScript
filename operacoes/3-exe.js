35682668bbd7f10aa1f5ef0708892b1d8242189b
Aline Souza <alinebeatriz115@gmail.com>
2025-05-26 14:48:28 -0300

    exercício avançado 3

--- operacoes/3-exe.js
+++ operacoes/3-exe.js
@@ -24,3 +24,25 @@ function simuladorINSS(salario){
     }
 }
 simuladorINSS(1900.50);
+
+//3. Simule um caixa eletrônico: dado um valor, informe quantas notas de 100, 50, 10 e 1 serão entregues.
+function caixaEletronico(valor) {
+    let notas100 = Math.floor(valor / 100);
+    valor = valor % 100;
+
+    let notas50 = Math.floor(valor / 50);
+    valor = valor % 50;
+
+    let notas10 = Math.floor(valor / 10);
+    valor = valor % 10;
+
+    let notas1 = Math.floor(valor / 1);
+
+    console.log("Notas de 100: " + notas100);
+    console.log("Notas de 50: " + notas50);
+    console.log("Notas de 10: " + notas10);
+    console.log("Notas de 1: " + notas1);
+}
+
+caixaEletronico(280);
+
